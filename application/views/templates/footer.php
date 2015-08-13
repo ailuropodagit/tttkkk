6b69e5145c6178cb317b6e6d0fe693e57f77a9b8
Wilkin <Wilkin@Wilkin-PC>
2015-08-13 22:41:10 +0800

    Merge origin/master
    Conflicts:
    	newEmptyPHP.php

---
+++ application/views/templates/footer.php
@@ -0,0 +1,5 @@
+<br/><br/>
+<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+        </body>
+</html>
+
---
+++ application/views/templates/header.php
@@ -0,0 +1,9 @@
+<html>
+        <head>
+                <title>Keppo</title>
+        </head>
+        <body>
+
+                <h1>Welcome to Keppo</h1>
+
+
