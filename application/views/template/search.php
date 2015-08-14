c71596004c0d435951f081f6fefba3a86a9b3d7c
Albert <Albert@192.168.1.12>
2015-08-14 22:26:19 +0800

    Merge origin/master
    Conflicts:
    	newEmptyPHP.php

--- application/views/template/search.php
+++ application/views/template/search.php
@@ -1,13 +1,25 @@
 <div id="wrapper">
-    <div id="header-search">
-        <div id="header-search-content">
-            <div id="header-search-content-box">
-                <div id="header-search-content-box-content">
-                    <input type="text" placeholder="Search: Tony Roma's, Vans, ChatTime">
-                    <select>
-                        <option>State</option>
-                    </select>
-                    <input type='submit' value='Search'>
+    <div id="search">
+        <div id="search-content">
+            <div id="search-content-box">
+                <div id="search-content-box-content">
+                    
+                    <div id="search-box-block1">
+                        <input type="text" placeholder="Search: Tony Roma's, Vans, ChatTime">
+                    </div>
+                    
+                    <div id="search-box-block2">
+                        <select>
+                            <option>State</option>
+                        </select>
+                    </div>
+                    
+                    <div id="search-box-block3">
+                        <input type='submit' value='Search'>
+                    </div>
+                    
+                    <div id="float-fix"></div>
+                    
                 </div>
             </div>
         </div>
