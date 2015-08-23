ea8917324ca8eb182c9a97ef5321c1bc0e1cf5d6
Albert <Albert@192.168.1.12>
2015-08-23 12:06:06 +0800

    Merge origin/master
    Conflicts:
    	application/config/config_keppo.php

--- application/views/page/about-us.php
+++ application/views/page/about-us.php
@@ -1,46 +1,38 @@
-<div id='wrapper'>
-    <div id='body'>
-        <div id='body-content'>
-            
-            <div id="about-us">
-                <h1>About Us</h1>
-                <div id="body-content-logo">
-                    <img src="<?php echo base_url(); ?>image/logo.png" id="body-content-logo-img">
-                </div>
-                <p>
-                    Keppo.my is publish on 19 July 2015, Founder by Jimmy See. His create this to help's SME Corp to increasing
-                    sale on advertising. Keppo.my is used hybird technilogy on media social faster way to user's to know more
-                    product and services.
-                </p>
-                <p style="margin: 0px 0px 35px 0px;">
-                    Keppo.my is B2B2C marketplace on media social to boots up Malaysia economic. This platform is face-to-face
-                    delivering information to user's. User;s able to collect candy from merchants (example: likes, Rating, view 
-                    advertisement, share on facebook or instagram). 
-                </p>            
-                <p>
-                    Fuyoo Advertising & services ( 002422825-U) <br/>
-                    No. 4, Jalan Industris Mas 5, Taman Mas, 47170, Puchong, Selangor.
-                </p>
-                <p>
-                    Tel: 03- 8061 9491 <br/>
-                    Fax: 03-8061 9257 <br/>
-                    E-mail: jimmysee2005@gmail.com <br/>
-                    <a href='home'>www.keppo.my</a>
-                </p>
-                <div id="contact-us-left-social-media-icon">
-                    <div id="contact-us-left-social-media-icon-facebook">
-                        <a href="https://www.facebook.com/" target="_blank"><i class="fa fa-facebook-square"></i></a>
-                    </div>
-                    <div id="contact-us-left-social-media-icon-instagram">
-                        <a href="https://instagram.com" target="_blank"><i class="fa fa-instagram"></i></a>
-                    </div>
-                    <div id="contact-us-left-social-media-icon-linkedin">
-                        <a href="https://www.linkedin.com/" target="_blank"><i class="fa fa-linkedin-square"></i></a>
-                    </div>
-                    <div id="float-fix"></div>
-                </div>
-            </div>
-            
+<div id="about-us">
+    <h1>About Us</h1>
+    <div id="body-content-logo">
+        <img src="<?php echo base_url(); ?>image/logo.png" id="body-content-logo-img">
+    </div>
+    <p>
+        Keppo.my is publish on 19 July 2015, Founder by Jimmy See. His create this to help's SME Corp to increasing
+        sale on advertising. Keppo.my is used hybird technilogy on media social faster way to user's to know more
+        product and services.
+    </p>
+    <p style="margin: 0px 0px 35px 0px;">
+        Keppo.my is B2B2C marketplace on media social to boots up Malaysia economic. This platform is face-to-face
+        delivering information to user's. User;s able to collect candy from merchants (example: likes, Rating, view 
+        advertisement, share on facebook or instagram). 
+    </p>            
+    <p>
+        Fuyoo Advertising & services ( 002422825-U) <br/>
+        No. 4, Jalan Industris Mas 5, Taman Mas, 47170, Puchong, Selangor.
+    </p>
+    <p>
+        Tel: 03- 8061 9491 <br/>
+        Fax: 03-8061 9257 <br/>
+        E-mail: jimmysee2005@gmail.com <br/>
+        <a href='home'>www.keppo.my</a>
+    </p>
+    <div id="contact-us-left-social-media-icon">
+        <div id="contact-us-left-social-media-icon-facebook">
+            <a href="https://www.facebook.com/" target="_blank"><i class="fa fa-facebook-square"></i></a>
         </div>
+        <div id="contact-us-left-social-media-icon-instagram">
+            <a href="https://instagram.com" target="_blank"><i class="fa fa-instagram"></i></a>
+        </div>
+        <div id="contact-us-left-social-media-icon-linkedin">
+            <a href="https://www.linkedin.com/" target="_blank"><i class="fa fa-linkedin-square"></i></a>
+        </div>
+        <div id="float-fix"></div>
     </div>
 </div>
--- application/views/simple_message.php
+++ application/views/simple_message.php
@@ -1,20 +1,14 @@
-<div id='wrapper'>
-    <div id='body'>
-        <div id='body-content'>
-            <div id="success-message">
-                <div id="success-message-title">
-                    <?php echo $simple_info['title']; ?>
-                </div>
-                <div id="success-message-subtitle">
-                    <?php echo $simple_info['sentence1']; ?>
-                </div>
-                <div id="success-message-paragraph">
-                    <?php echo $simple_info['sentence2']; ?>
-                </div>
-                <div id="success-message-link">
-                    <?php echo anchor($simple_info['back_page_url'], $simple_info['back_page']); ?>
-                </div>
-            </div>
-        </div>
+<div id="success-message">
+    <div id="success-message-title">
+        <?php echo $simple_info['title']; ?>
+    </div>
+    <div id="success-message-subtitle">
+        <?php echo $simple_info['sentence1']; ?>
+    </div>
+    <div id="success-message-paragraph">
+        <?php echo $simple_info['sentence2']; ?>
+    </div>
+    <div id="success-message-link">
+        <?php echo anchor($simple_info['back_page_url'], $simple_info['back_page']); ?>
     </div>
 </div>
