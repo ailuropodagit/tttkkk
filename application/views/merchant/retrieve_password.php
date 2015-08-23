0784be7eeeb034149868e8857956a9f7745b4875
Albert <Albert@192.168.1.12>
2015-08-23 11:21:03 +0800

    Merge origin/master
    Conflicts:
    	application/config/config_keppo.php

--- application/views/merchant/retrieve_password.php
+++ application/views/merchant/retrieve_password.php
@@ -1,23 +1,19 @@
-<div id='wrapper'>
-    <div id='body'>
-        <div id='body-content'>   
-             <div id="login">
-            <h1><?php echo lang('forgot_password_heading'); ?></h1>
-            <p><?php echo sprintf(lang('forgot_password_subheading'), $identity_label); ?></p>
+<div id="login">
+    
+    <h1><?php echo lang('forgot_password_heading'); ?></h1>
+    <p><?php echo sprintf(lang('forgot_password_subheading'), $identity_label); ?></p>
 
-            <div id="infoMessage"><?php echo $message; ?></div>
+    <div id="infoMessage"><?php echo $message; ?></div>
 
-            <?php echo form_open("merchant/retrieve_password"); ?>
+    <?php echo form_open("merchant/retrieve_password"); ?>
 
-            <p>
-                <label for="username_email"><?php echo sprintf(lang('forgot_password_username_email_label'), $identity_label); ?>:</label> <br />
-                <?php echo form_input($username_email); ?>
-            </p>
+    <p>
+        <label for="username_email"><?php echo sprintf(lang('forgot_password_username_email_label'), $identity_label); ?>:</label> <br />
+        <?php echo form_input($username_email); ?>
+    </p>
 
-            <p><?php echo form_submit('submit', lang('forgot_password_submit_btn')); ?></p>
+    <p><?php echo form_submit('submit', lang('forgot_password_submit_btn')); ?></p>
 
-            <?php echo form_close(); ?>
-             </div>
-        </div>
-    </div>
+    <?php echo form_close(); ?>
+
 </div>
--- application/views/user/login.php
+++ application/views/user/login.php
@@ -1,57 +1,49 @@
-<div id='wrapper'>
-    <div id='body'>
-        <div id='body-content'>
-                        
-            <div id="login">
-                
-                <div id='login-link'><a href='<?php echo base_url(); ?>merchant/login'>Merchant Login</a></div>
-                <div id='float-fix'></div>
+<div id="login">
 
-                <div id='login-title'>Log In</div>
-                
-                <div id='login-subtitle'>Don't have an account? <a href='<?php echo base_url(); ?>user/register'>Sign Up</a></div>
+    <div id='login-link'><a href='<?php echo base_url(); ?>merchant/login'>Merchant Login</a></div>
+    <div id='float-fix'></div>
 
-                <div id="infoMessage"><?php echo $message; ?></div>
+    <div id='login-title'>Log In</div>
 
-                <div id='login-facebook-icon'><img src='<?php echo base_url(); ?>image/facebook-icon.png'></div>
+    <div id='login-subtitle'>Don't have an account? <a href='<?php echo base_url(); ?>user/register'>Sign Up</a></div>
 
-                <div id='login-horizontal-line'></div>
-                
-                <?php echo form_open("user/login"); ?>       
-                
-                <div id='login-form'>
-                    
-                    <div id='login-form-each'>
-                        <div id='login-form-each-label'><?php echo lang('login_identity_label', 'identity'); ?></div>
-                        <div id='login-form-each-input'><?php echo form_input($identity); ?></div>
-                    </div>
+    <div id="infoMessage"><?php echo $message; ?></div>
 
-                    <div id='login-form-each'>
-                        <div id='login-form-each-label'><?php echo lang('login_password_label', 'password'); ?></div>
-                        <div id='login-form-each-input'><?php echo form_input($password); ?></div>
-                    </div>
+    <div id='login-facebook-icon'><img src='<?php echo base_url(); ?>image/facebook-icon.png'></div>
 
-                    <div id='login-form-remember-me-forgot-password'>
-                        <div id='login-form-remember-me'>
-                            <?php echo form_checkbox('remember', '1', FALSE, 'id="remember"'); ?> 
-                            <?php echo lang('login_remember_label', 'remember'); ?>
-                        </div>
-                        <div id='login-form-forgot-password'>
-                            <a href="retrieve-password"><?php echo lang('login_forgot_password'); ?></a>
-                        </div>
-                        <div id='float-fix'></div>
-                    </div>
+    <div id='login-horizontal-line'></div>
 
-                    <div id='login-form-submit'><?php echo form_submit('submit', lang('login_submit_btn')); ?></div>
-                    
-                </div>
-                    
-                <?php echo form_close(); ?>
+    <?php echo form_open("user/login"); ?>       
 
-                <div id='login-tnc'>by clicking Log In, Facebook you agree to our new <a href='<?php echo base_url(); ?>terms-and-conditions' target='_blank'>T&C's</a></div>
-                
-            </div>
+    <div id='login-form'>
 
+        <div id='login-form-each'>
+            <div id='login-form-each-label'><?php echo lang('login_identity_label', 'identity'); ?></div>
+            <div id='login-form-each-input'><?php echo form_input($identity); ?></div>
         </div>
+
+        <div id='login-form-each'>
+            <div id='login-form-each-label'><?php echo lang('login_password_label', 'password'); ?></div>
+            <div id='login-form-each-input'><?php echo form_input($password); ?></div>
+        </div>
+
+        <div id='login-form-remember-me-forgot-password'>
+            <div id='login-form-remember-me'>
+                <?php echo form_checkbox('remember', '1', FALSE, 'id="remember"'); ?> 
+                <?php echo lang('login_remember_label', 'remember'); ?>
+            </div>
+            <div id='login-form-forgot-password'>
+                <a href="retrieve-password"><?php echo lang('login_forgot_password'); ?></a>
+            </div>
+            <div id='float-fix'></div>
+        </div>
+
+        <div id='login-form-submit'><?php echo form_submit('submit', lang('login_submit_btn')); ?></div>
+
     </div>
+
+    <?php echo form_close(); ?>
+
+    <div id='login-tnc'>by clicking Log In, Facebook you agree to our new <a href='<?php echo base_url(); ?>terms-and-conditions' target='_blank'>T&C's</a></div>
+
 </div>
