a1a493237da2940f74a736083b0a2947fcf2383f
Wilkin <Wilkin@Wilkin-PC>
2015-08-27 19:10:58 +0800

    Merge origin/master
    Conflicts:
    	application/controllers/Auth.php
    	application/models/Ion_auth_model.php

--- application/views/template/layout_management.php
+++ application/views/template/layout_management.php
@@ -1,21 +1,27 @@
 <?php $this->load->view('template/header'); ?>
 
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<?php foreach ($css_files as $file): ?>
+    <link type="text/css" rel="stylesheet" href="<?php echo $file; ?>" />
+<?php endforeach; ?>
+<?php foreach ($js_files as $file): ?>
+    <script src="<?php echo $file; ?>"></script>
+<?php endforeach; ?>
+
 <div id='wrapper'>
     <div id='body'>
         <div id='body-content'>
-            <meta charset="utf-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <?php foreach ($css_files as $file): ?>
-                <link type="text/css" rel="stylesheet" href="<?php echo $file; ?>" />
-            <?php endforeach; ?>
-            <?php foreach ($js_files as $file): ?>
-                <script src="<?php echo $file; ?>"></script>
-            <?php endforeach; ?>
-            <div>
-                <?php echo $output; ?>
+            <?php $this->load->view('template/sidebar'); ?>
+
+            <!--BODY LEFT MAIN-->
+            <div id="body-left-main">
+                <div id="body-left-main-content">
+                    <?php echo $output; ?>
+                </div>
             </div>
         </div>
     </div>
 </div>
-
+<div id="float-fix"></div>
 <?php $this->load->view('template/footer'); ?>
--- application/views/template/sidebar.php
+++ application/views/template/sidebar.php
@@ -1,1 +1,14 @@
-sidebar
+<!--BODY RIGHT SIDEBAR-->
+            <div id="body-right-sidebar">
+                <ul>
+                    <li><a href='<?php echo base_url(); ?>merchant/dashboard' class='body-right-sidebar-bar body-right-sidebar-bar-active'>Dashboard</a></li>
+                    <li><a href='<?php echo base_url(); ?>merchant/profile' class='body-right-sidebar-bar'>Profile</a></li>
+                    <li><a href='' class='body-right-sidebar-bar'>Password</a></li>
+                    <li><a href='' class='body-right-sidebar-bar'>Hot Deal Advertise</a></li>
+                    <li><a href='' class='body-right-sidebar-bar'>Candie Promotion</a></li>
+                    <li><a href='' class='body-right-sidebar-bar'>Picture</a></li>
+                    <li><a href='' class='body-right-sidebar-bar'>User Redemption</a></li>
+                    <li><a href='' class='body-right-sidebar-bar'>Analysis Report</a></li>
+                    <li><a href='' class='body-right-sidebar-bar'>Payment</a></li>
+                </ul>
+            </div>
