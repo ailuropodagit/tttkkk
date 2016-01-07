e75a5fef4e31a24fa71778f1ca3e1c029a0eccfc
Wilkin <Wilkin@Wilkin-PC>
2016-01-07 21:53:02 +0800

    Merge origin/master
    Conflicts:
    	application/views/share/redemption_grid_list4.php

--- application/views/user/upload_image.php
+++ application/views/user/upload_image.php
@@ -1,4 +1,6 @@
 <script type="text/javascript" src="<?php echo base_url() ?>js/jquery.ajaxfileupload.js"></script>
+<script type="text/javascript" src="<?php echo base_url() ?>js/multiple-upload/jquery.fileuploadmulti.min.js"></script>
+<link rel="stylesheet" href="<?php echo base_url('js/multiple-upload/uploadfilemulti.css') ?>">
 
 <script type="text/javascript">
     $(document).ready(function(){        
@@ -6,22 +8,57 @@
         var temp_folder = '<?php echo $temp_folder ?>';
         for (var counter = 0; counter < <?php echo $box_number ?>; counter++) {
             $('#image-file-'+counter).ajaxfileupload({
-      'action': 'http://' + $(location).attr('hostname') + keppo_path + 'all/upload_image_temp',
-      'params': {
-        'file_name': 'image-file-'+counter,
-        'image_box_id': 'image_url-'+counter
-      },
-      'onComplete': function(response) {
-        //alert(JSON.stringify(response));
-        var post_url = 'http://' + $(location).attr('hostname') + keppo_path + temp_folder;
-        //var post_image = "<img src='" + post_url + response + "'>";
-        var post_image = post_url + response[0];
-        //$( '#upload-for-merchant-form-photo-box' ).html(post_image);
-        $('img#'+ response[1]).attr('src', post_image);
-      }
+                'action': 'http://' + $(location).attr('hostname') + keppo_path + 'all/upload_image_temp',
+                'params': {
+                  'file_name': 'image-file-'+counter,
+                  'image_box_id': 'image_url-'+counter
+                },
+                'onComplete': function(response) {
+                  //alert(JSON.stringify(response));
+                  var post_url = 'http://' + $(location).attr('hostname') + keppo_path + temp_folder;
+                  //var post_image = "<img src='" + post_url + response + "'>";
+                  var post_image = post_url + response[0];
+                  //$( '#upload-for-merchant-form-photo-box' ).html(post_image);
+                  $('img#'+ response[1]).attr('src', post_image);
+                }
+            });
+        }
+        
+        var temp_folder_cut = '<?php echo $temp_folder_cut ?>';
+        var empty_image = '<?php echo $empty_image ?>';
+        var settings = {
+	url: 'http://' + $(location).attr('hostname') + keppo_path + 'all/upload_image_temp_multiple',
+	method: "POST",
+	allowedTypes:"gif,jpg,png,bmp,ico,jpeg,jpe",
+	fileName: "myfile",
+	multiple: true,
+        maxFileCount: <?php echo $box_number ?>,
+        showDone: false,
+        showStatusAfterSuccess: false,
+	onSuccess:function(files,data,xhr)
+	{
+		//$("#status").html("<font color='green'>Upload is success</font>");
+                for (var counter = 0; counter < <?php echo $box_number ?>; counter++) {
+                var images = $('img#image_url-'+counter).attr('src');
+                    if (images.indexOf(empty_image) >= 0){
+                        data = data.replace(/\"/g, '');
+                        $('img#image_url-'+counter).attr('src', 'http://' + $(location).attr('hostname') + keppo_path + temp_folder_cut + data);
+                        break;
+                    }                
+                }
+                
+        },
+        afterUploadAll:function()
+        {
+                //alert("all images uploaded!!");
+        },
+	onError: function(files,status,errMsg)
+	{		
+		$("#status").html("<font color='red'>Upload is Failed</font>");
+	}
+        }
+        $("#mulitplefileuploader").uploadFile(settings);        
     });
-    }
-});
 </script>
 
 <?php
@@ -35,11 +72,16 @@ if(isset($message))
 <div id="upload-for-merchant">
     <h1>Upload Picture for My Album</h1>
     <div id="upload-for-merchant-content">
-        <div id="upload-for-merchant-merchant-album">
+        <div id="upload-for-merchant-merchant-album" style="float:left">
             <?php
             $this->load->view('all/album_user_sub_menu');
-            ?>
+            ?>                       
         </div>
+        <div style="float:right">
+              Upload Multiple (Max 5) : <div id="mulitplefileuploader">Upload</div>
+              <div id="status"></div>
+        </div>
+        <div id="float-fix"></div>
         <div id="upload-for-merchant-upload-image-note" style="display:none">
             Upload Image Rule : <?php echo $this->config->item('upload_guide_image') ?>
         </div>
