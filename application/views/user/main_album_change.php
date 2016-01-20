a40d99973d85e6226a056a41e975e78463cbb797
Wilkin <Wilkin@Wilkin-PC>
2016-01-20 20:40:45 +0800

    Merge origin/master
    Conflicts:
    	application/views/user/main_album_change.php

--- application/views/user/main_album_change.php
+++ application/views/user/main_album_change.php
@@ -41,21 +41,4 @@ if(isset($message))
             </div>
             <?php echo form_close(); ?>
         </div>
-        <div id="float-fix"></div>
-        
-<?php echo form_open(uri_string()); ?>
-<div id='profile-info-form-each-label'><?php echo lang("main_album_title_label"); ?></div>
-<div id='profile-info-form-each-input'><?php echo form_input($album_title); ?></div>
-
-<?php
-echo form_hidden($edit_id);
-?>
-
-<div id='profile-info-form-submit'>              
-    <button name="button_action" type="submit" value="back">Back</button>
-    <button name="button_action" type="submit" value="save" onclick="return confirm('Confirm that information is correct before save it?')">Save</button>
-</div>
-
-<?php echo form_close(); ?>
-    
-<div id="float-fix"></div>
+        <div id="float-fix"></div>       
