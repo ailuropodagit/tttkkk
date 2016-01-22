d581d7c5d90777d227ae48dd66dce1ac2e50c94e
Albert <Albert@192.168.1.11>
2016-01-22 23:18:24 +0800

    Merge origin/master
    Conflicts:
    	application/views/all/promotion.php

--- application/controllers/All.php
+++ application/controllers/All.php
@@ -1468,11 +1468,7 @@ class All extends CI_Controller
                 }
                 $this->data['home_search_merchant'] = $this->m_custom->home_search_merchant($search_value, $state_id);
                 $this->data['home_search_hotdeal'] = $this->m_custom->home_search_hotdeal($search_value, $state_id);
-                $this->data['home_search_promotion'] = $this->m_custom->home_search_promotion($search_value, $state_id);
-                
-                $test = $this->m_custom->home_search_hotdeal($search_value, $state_id);
-                print_r($test);
-                
+                $this->data['home_search_promotion'] = $this->m_custom->home_search_promotion($search_value, $state_id);      
                 $this->data['state_name'] = "";
                 if ($state_id != 0)
                 {
