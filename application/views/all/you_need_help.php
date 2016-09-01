af1c0128c154a523287f987cb132cc734aafc501
Albert <Albert@192.168.1.12>
2016-09-01 20:11:42 +0800

    Merge origin/master
    Conflicts:
    	application/views/template/header.php

--- application/views/all/you_need_help.php
+++ application/views/all/you_need_help.php
@@ -2,4 +2,47 @@
 $video_link = $_POST['video_link'];
 ?>
 
-<?php echo $video_link ?>
+<div id="you-need-help-inner">
+    <?php 
+    if($video_link == 1)
+    {
+        ?><iframe width="100%" src="https://www.youtube.com/embed/qB2rL-Hf40g" frameborder="0" allowfullscreen></iframe><?php
+    }
+    elseif($video_link == 2)
+    {
+
+    }
+    elseif($video_link == 3)
+    {
+        ?><iframe width="100%" src="https://www.youtube.com/embed/fLfcQc7lO3g" frameborder="0" allowfullscreen></iframe><?php
+    } 
+    elseif($video_link == 4)
+    {
+        ?><iframe width="100%" src="https://www.youtube.com/embed/djPrdDRmoqw" frameborder="0" allowfullscreen></iframe><?php
+    } 
+    elseif($video_link == 5)
+    {
+        ?><iframe width="100%" src="https://www.youtube.com/embed/Xy9qQoQbZHw" frameborder="0" allowfullscreen></iframe><?php
+    } 
+    elseif($video_link == 6)
+    {
+        ?><iframe width="100%" src="https://www.youtube.com/embed/97J8lQhw1VI" frameborder="0" allowfullscreen></iframe><?php
+    } 
+    elseif($video_link == 7)
+    {
+        ?><iframe width="100%" src="https://www.youtube.com/embed/RGzdcaji3Uc" frameborder="0" allowfullscreen></iframe><?php
+    } 
+    elseif($video_link == 8)
+    {
+        ?><iframe width="100%" src="https://www.youtube.com/embed/76UMrEByHgc" frameborder="0" allowfullscreen></iframe><?php
+    } 
+    elseif($video_link == 9)
+    {
+        ?><iframe width="100%" src="https://www.youtube.com/embed/8QJoPiFbaJE" frameborder="0" allowfullscreen></iframe><?php
+    } 
+    elseif($video_link == 10)
+    {
+        ?><iframe width="100%" src="https://www.youtube.com/embed/gt9lYkwa2go" frameborder="0" allowfullscreen></iframe><?php
+    } 
+    ?>
+</div>
